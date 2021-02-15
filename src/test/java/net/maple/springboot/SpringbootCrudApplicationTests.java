f4d29919bbbe47c606dc8f17d2196ae9f6594771
Sakura-2019 <738237672@qq.com>
2021-02-15 11:31:58 +0800

    初始化项目

---
+++ src/test/java/net/maple/springboot/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package net.maple.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
